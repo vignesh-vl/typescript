86f960ac8de78d3f30c886b6639ced6b2f5e9e5b
vignesh-vl <kirubhavikki@gmail.comm>
2022-08-10 13:06:09 +0530

    demo typescript file

---
+++ demo_typescript.js
@@ -0,0 +1,6 @@
+var stud_name = " vignesh ";
+// console.log(stud_name);
+var input_node_list = document.querySelectorAll("input");
+input_node_list.forEach(function (input) {
+    console.log(input);
+});
---
+++ demo_typescript.ts
@@ -0,0 +1,6 @@
+const stud_name = " vignesh "
+// console.log(stud_name);
+const input_node_list =  document.querySelectorAll("input");
+input_node_list.forEach(input => {
+    console.log(input);
+})
